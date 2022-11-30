ad651d267b473ac599c7f249a8345876a2b63d66
ewanhughes26 <79140539+ewanhughes26@users.noreply.github.com>
2022-11-30 14:36:59 +0000

    Comments and formatting.

--- src/main/java/com/example/fxapp/HelloApplication.java
+++ src/main/java/com/example/fxapp/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application
     {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 640);
-        stage.setTitle("Hello Trevor!");
+        stage.setTitle("Hello there!");
         stage.setScene(scene);
         stage.show();
     }
